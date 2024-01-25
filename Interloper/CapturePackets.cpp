3f4913a1da39599464b312408c2957674668a1e7
LordCat <kristianalexball@gmail.com>
2024-01-25 19:32:17 +0000

    CaptuePacket finished. Well I mean it compiles. Need to test

--- Interloper/CapturePackets.cpp
+++ Interloper/CapturePackets.cpp
@@ -1,92 +1,84 @@
 #include "CapturePackets.h"
 #include <iostream>
 #include <pcap.h>
-#include <Windows.h>	 
+#include <Windows.h>
 
 capturePacket::capturePacket(const std::string& sInterface) : selectedInterface(sInterface) {}
 
-void capturePacket::CaptureCoroutine::awaiter::await_suspend(std::coroutine_handle<> coroutine) const {
-	parent.startCaptureAsync(coroutine);
-}
-
 capturePacket::CaptureCoroutine capturePacket::startCapture() {
-	return CaptureCoroutine{ *this, std::coroutine_handle<CaptureCoroutine::promise_type>::from_promise(CaptureCoroutine::promise_type) };
+    co_return;
 }
 
-
-
-
-//actuall function for capturing packets
+// Actual function for capturing packets
 void capturePacket::startCaptureAsync(std::coroutine_handle<> coroutine) {
-	char errbuf[PCAP_ERRBUF_SIZE];
-	pcap_t* pcapHandle;
+    char errbuf[PCAP_ERRBUF_SIZE];
+    pcap_t* pcapHandle;
 
-	//open the network interface for capturing
-	pcapHandle = pcap_open_live(selectedInterface.c_str(), BUFSIZ, 1, 1000, errbuf);
+    // Open the network interface for capturing
+    pcapHandle = pcap_open_live(selectedInterface.c_str(), BUFSIZ, 1, 1000, errbuf);
 
-	if (pcapHandle == nullptr) {
-		std::cerr << "Error Opening Interface: " << errbuf << std::endl;
-		coroutine.destroy(); //destory the coroutine on error 
-		return;
-	}
+    if (pcapHandle == nullptr) {
+        std::cerr << "Error Opening Interface: " << errbuf << std::endl;
+        coroutine.destroy(); // Destroy the coroutine on error
+        return;
+    }
 
-	std::cout << "Packet Capture started on interface: " << selectedInterface << std::endl;
+    std::cout << "Packet Capture started on interface: " << selectedInterface << std::endl;
 
-	//option for capture rate 
-	std::cout << "Choose capture rate:\n";
-	std::cout << "1: 1 packet/sec\n";
-	std::cout << "2: 3 packet/sec\n";
-	std::cout << "3: 5 packet/sec\n";
+    // Option for capture rate
+    std::cout << "Choose capture rate:\n";
+    std::cout << "1: 1 packet/sec\n";
+    std::cout << "2: 3 packet/sec\n";
+    std::cout << "3: 5 packet/sec\n";
 
-	char choice;
-	std::cin >> choice;
+    char choice;
+    std::cin >> choice;
 
-	int packetRate;
-	switch (choice) {
-	case '1':
-		packetRate = 1;
-		break;
-	case '2':
-		packetRate = 3;
-		break;
-	case '3':
-		packetRate = 5;
-		break;
-	default:
-		std::cerr << "Invalid choice. Exiting. \n";
-		pcap_close(pcapHandle);
-		coroutine.destroy();
-		return;
-	}
+    int packetRate;
+    switch (choice) {
+    case '1':
+        packetRate = 1;
+        break;
+    case '2':
+        packetRate = 3;
+        break;
+    case '3':
+        packetRate = 5;
+        break;
+    default:
+        std::cerr << "Invalid choice. Exiting. \n";
+        pcap_close(pcapHandle);
+        coroutine.destroy();
+        return;
+    }
 
-	//captue loop
-	while (true) {
-		struct pcap_pkthdr header;
-		const u_char* packet;
+    // Capture loop
+    while (true) {
+        struct pcap_pkthdr header;
+        const u_char* packet;
 
-		//capture packet
-		packet = pcap_next(pcapHandle, &header);
+        // Capture packet
+        packet = pcap_next(pcapHandle, &header);
 
-		if (packet != nullptr) {
-			//output packet informaton
-			std::cout << "Packer captured on " << selectedInterface << ".\n Time: " << header.ts.tv_sec << "." << header.ts.tv_usec << "\n Lenght: " << header.len << std::endl;
+        if (packet != nullptr) {
+            // Output packet information
+            std::cout << "Packet captured on " << selectedInterface << ".\n Time: " << header.ts.tv_sec << "." << header.ts.tv_usec << "\n Length: " << header.len << std::endl;
 
-			//Sleep  for the specificed captue wait 
-			Sleep(1000 / packetRate);
-		}
+            // Sleep for the specified capture wait
+            Sleep(1000 / packetRate);
+        }
 
-		//Check 'S' press to stop capture
-		if (std::cin.peek() == 'S' || std::cin.peek() == 's') {
-			std::cout << "Stopping capture... \n";
-			break;
-		}
-	}
+        // Check 'S' press to stop capture
+        if (std::cin.peek() == 'S' || std::cin.peek() == 's') {
+            std::cout << "Stopping capture... \n";
+            break;
+        }
+    }
 
+    // Close the capture handle
+    pcap_close(pcapHandle);
 
-	//close the capture handle
-	pcap_close(pcapHandle);
+    std::cout << "Capture stopped. Returning to the main menu. \n";
 
-	std::cout << "Capture stopped. Returning to main menu. \n";
-
-	coroutine.destroy();
+    coroutine.destroy();
 }
--- Interloper/CapturePackets.h
+++ Interloper/CapturePackets.h
@@ -7,52 +7,49 @@
 #include <coroutine>
 
 class capturePacket {
-public: 
-	//constructor
-	capturePacket(const std::string& sInterface);
+public:
+    // Constructor
+    capturePacket(const std::string& sInterface);
 
-	//corountine to start packet capture
-	struct CaptureCoroutine {
-		capturePacket& parent;
+    // Coroutine to start packet capture
+    struct CaptureCoroutine {
+        class promise_type {
+        public:
+            std::suspend_never initial_suspend() { return {}; }
+            std::suspend_never final_suspend() noexcept { return {}; }
 
-		//promise type for co-routine
-		struct promise_type {
-			std::suspend_never intital_suspend() { return {}; }
-			std::suspend_never final_suspend() noexcept { return {}; }
+            void return_void() {}
+            void unhandled_exception() {}
 
-			void return_void() {}
-			void unhandled_expection() {}
-		};
+            // This function constructs an instance of CaptureCoroutine
+            CaptureCoroutine get_return_object() {
+                return CaptureCoroutine{ std::coroutine_handle<promise_type>::from_promise(*this) };
+            }
+        };
 
-		//co-routien handle
-		std::coroutine_handle<promise_type> coro_Handle;
+        // Coroutine handle
+        std::coroutine_handle<promise_type> coro_Handle;
 
-		//co-routine constructor 
-		CaptureCoroutine(capturePacket& p, std::coroutine_handle<promise_type> coroutine)
-			: parent(p), coro_Handle(coroutine) {}
+        // Coroutine constructor 
+        CaptureCoroutine(std::coroutine_handle<promise_type> coroutine)
+            : coro_Handle(coroutine) {}
 
-		//awaiting able type for suspending and resuming the co-rountine
-		struct awaiter {
-			capturePacket& parent;
-			bool await_ready() const { return false; }
-			void await_suspend(std::coroutine_handle<> coroutine) const;
-			void await_resume() const {}
-		};
+        // Awaiting able type for suspending and resuming the coroutine
+        struct awaiter {
+            bool await_ready() const { return false; }
+            void await_suspend(std::coroutine_handle<> coroutine) const;
+            void await_resume() const {}
+        };
+    };
 
-		awaiter operator co_await() const { return { parent }; }
-
-	};
-
-
-
-	//function to start capture
-	CaptureCoroutine startCapture();
+    // Function to start capture
+    CaptureCoroutine startCapture();
 
 private:
-	std::string selectedInterface;
+    std::string selectedInterface;
 
-	// Function to perform the actual packet capture
-	void startCaptureAsync(std::coroutine_handle<> coroutine);
+    // Function to perform the actual packet capture
+    void startCaptureAsync(std::coroutine_handle<> coroutine);
 };
 
 #endif
